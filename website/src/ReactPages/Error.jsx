8944c75a0b92d3db51a1702b5c56e0116c31550a
Lex Truong <lext@uci.edu>
2025-02-27 23:55:07 -0800

    replaced error page background

--- website/src/ReactPages/Error.jsx
+++ website/src/ReactPages/Error.jsx
@@ -23,24 +23,22 @@ export default function Error() {
         gap: 5,
         textAlign: "center",
         alignItems: "center",
-        backgroundImage: "url('src/assets/temp.jpeg')",
-        backgroundColor: "rgba(255,255,255,.6)",
+        backgroundImage: "url('src/assets/woah.jpeg')",
+        backgroundColor: "rgba(255,255,255,0.3)",
         backgroundBlendMode: "lighten",
         backgroundSize: "cover",
       }}
     >
       <Box>
         <Typography
-          // fontFamily="fontFamily"
-          fontSize={70}
+          fontSize={75}
           fontWeight={550}
         >
           Page Not Found
         </Typography>
         <Typography
-          // fontFamily="fontFamily"
-          fontSize={35}
-          fontWeight={500}
+          fontSize={40}
+          fontWeight={510}
         >
           We can't seem to find the page you're looking for.
         </Typography>
@@ -54,11 +52,9 @@ export default function Error() {
           height: 60,
           borderRadius: 3,
           backgroundColor: "#E50000",
-          // position: "absolute"
         }}
       >
         <Typography
-          // fontFamily="fontFamilyHeader"
           fontSize={20}
           fontWeight={500}
         >
@@ -66,51 +62,5 @@ export default function Error() {
         </Typography>
       </Button>
     </Box>
-    // <div style={{
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     justifyContent: "center",
-    //     width: '100%',
-    //     height: '100vh',
-    //     gap: 30,
-    //     textAlign: "center",
-    //     alignItems: "center",
-    //     width: '100%',
-    //     height: '100vh',
-    //     backgroundImage: "url('src/assets/temp.jpeg')",
-    //     backgroundColor: "rgba(255,255,255,.6)",
-    //     backgroundBlendMode: "lighten",
-    //     backgroundSize: "cover"
-    // }}>
-
-    //     <Container>
-    //         <Typography
-    //             // fontFamily="fontFamily"
-    //             fontSize={70}
-    //             fontWeight={550}
-    //         >Page Not Found</Typography>
-    //         <Typography
-    //             // fontFamily="fontFamily"
-    //             fontSize={35}
-    //             fontWeight={500}
-    //         >We can't seem to find the page you're looking for.</Typography>
-    //     </Container>
-
-    //     <Button variant="contained" href={"/"} sx={{
-    //         width: 300,
-    //         height: 60,
-    //         borderRadius: 3,
-    //         backgroundColor: "#E50000",
-    //         // position: "absolute"
-    //     }}>
-    //         <Typography
-    //             // fontFamily="fontFamilyHeader"
-    //             fontSize={20}
-    //             fontWeight={500}
-    //         >Back To Home
-    //         </Typography>
-    //     </Button>
-
-    // </div>
   );
 }
