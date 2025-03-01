45d7bdd2df34029dd2513214e78ef9e22683f0a2
Lex Truong <lext@uci.edu>
2025-02-28 16:22:43 -0800

    formatted about page

--- website/src/ReactPages/AboutPage.jsx
+++ website/src/ReactPages/AboutPage.jsx
@@ -1,4 +1,14 @@
-import { Avatar, Box, Button, ThemeProvider, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  ThemeProvider,
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea
+} from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import theme from "../theme";
 
@@ -27,107 +37,216 @@ export default function AboutPage() {
           justifyContent: "center",
           alignItems: "center",
           width: "100vw",
-          overflow: "hidden",
+          overflow: "hidden"
         }}
       >
-        <Box
-          component="img"
-          src="src/assets/temp.jpeg"
-          alt="hero"
-          sx={{
-            // height: "50vh",
-            width: "100%",
-            // objectFit: "cover",
-            objectFit: "contain"
-          }}
-        />
+        {/* WIND GUSH ON CLICK */}
         <Box
           component="div"
           sx={{
-            height: "100px",
+            backgroundImage: "url('src/assets/temp.jpeg')",
+            height: "70vh",
             width: "100%",
-            backgroundColor: "secondary.main",
+            backgroundPosition: "center center",
+            backgroundSize: "cover",
             display: "flex",
             alignItems: "center",
-            justifyContent: "center"
+            justifyContent: "center",
           }}
         >
-          <Typography
-            fontSize={30}
-            fontWeight={300}
+          <Box
+            component="div"
             sx={{
-              color: "white",
-              fontStyle: "italic"
+              height: 150,
+              width: 500,
+              backgroundColor: "secondary.main",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center",
+              border: "5px solid black",
+              padding: 10
             }}
           >
-            Let the Southern Wind Guide You
-          </Typography>
+            <Typography
+              fontSize={30}
+              fontWeight={300}
+              sx={{
+                color: "white",
+                fontStyle: "italic"
+              }}
+            >
+              Let the Southern Wind Guide You
+            </Typography>
+          </Box>
         </Box>
+
         <Box
           sx={{
             display: "flex",
-            flexDirection: collapsedView ? "row" : "column",
+            flexDirection: "column",
             padding: "2rem",
             backgroundColor: "#f6f6f6",
             width: "100%",
             justifyContent: "center",
             alignItems: "center",
             gap: "2rem",
+            margin: 0,
+            padding: 0
           }}
         >
           <Box
             sx={{
-              
+              paddingTop: 2,
+              paddingBottom: 5,
+              paddingRight: collapsedView ? 20 : 10,
+              paddingLeft: collapsedView ? 20 : 10,
             }}
           >
-            <h1>Mission</h1>
-            <Typography>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit molestiae nam delectus praesentium facere? Minus accusamus delectus obcaecati eos, assumenda soluta alias, quas unde id numquam saepe facere et sequi?
-            </Typography>
-            <h1>About</h1>
-            <Typography>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit molestiae nam delectus praesentium facere? Minus accusamus delectus obcaecati eos, assumenda soluta alias, quas unde id numquam saepe facere et sequi?
-            </Typography>
-            <h1>History</h1>
-            <Typography>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit molestiae nam delectus praesentium facere? Minus accusamus delectus obcaecati eos, assumenda soluta alias, quas unde id numquam saepe facere et sequi?
-            </Typography>
-            <h1>Our Coordinators</h1>
+
+            <Box>
+              <h1>Mission</h1>
+              <Typography>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit molestiae nam delectus praesentium facere? Minus accusamus delectus obcaecati eos, assumenda soluta alias, quas unde id numquam saepe facere et sequi?
+              </Typography>
+            </Box>
+
             <Box
               sx={{
-                display: "flex",
-                flexDirection: collapsedView ? "row" : "column",
-                justifyContent: "center",
-                alignItems: "center",
-                gap: 5
+                backgroundColor: "beige",
+                padding: 10,
+                paddingTop: 2,
+                marginTop: 5,
+                textAlign: "center"
               }}
             >
-              {coordinators.map(coord => (
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: 2
-                  }}
-                >
-                  <Avatar
-                    alt={coord.alt}
-                    src={coord.src}
-                    sx={{
-                      width: 300,
-                      height: 300
-                    }}
-                  />
-                  <Typography
-                    fontSize={25}
-                    fontWeight={500}
-                  >
-                    {coord.alt}
-                  </Typography>
-                </Box>
+              <h1>About</h1>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: collapsedView ? "row" : "column",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  gap: 5
+                }}
+              >
+                <Card sx={{maxWidth: 400}}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="300"
+                      image="src/assets/lion-lineup-5.JPG"
+                    />
+                    <CardContent>
+                      <Typography fontSize={25} fontWeight={600}>
+                        TRADITION
+                      </Typography>
+                      <Typography>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis reiciendis, perspiciatis voluptas hic saepe provident, quis sit sequi corporis sunt earum. Sit quos cum nulla officiis. Quae, repudiandae dolore!
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{maxWidth: 400}}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="300"
+                      image="src/assets/lion-lineup-5.JPG"
+                    />
+                    <CardContent>
+                      <Typography fontSize={25} fontWeight={600}>
+                        COMMUNITY
+                      </Typography>
+                      <Typography>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis reiciendis, perspiciatis voluptas hic saepe provident, quis sit sequi corporis sunt earum. Sit quos cum nulla officiis. Quae, repudiandae dolore!
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{maxWidth: 400}}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="300"
+                      image="src/assets/lion-lineup-5.JPG"
+                    />
+                    <CardContent>
+                      <Typography fontSize={25} fontWeight={600}>
+                        BONDS
+                      </Typography>
+                      <Typography>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis reiciendis, perspiciatis voluptas hic saepe provident, quis sit sequi corporis sunt earum. Sit quos cum nulla officiis. Quae, repudiandae dolore!
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Box>
+            </Box>
 
-              ))}
+            <Box>
+              <h1>History</h1>
+              {/* OLD TEAM PHOTO */}
+              <Box
+                component="div"
+                sx={{
+                  backgroundImage: "url('src/assets/lion-lineup-3.JPG')",
+                  height: "70vh",
+                  width: "100%",
+                  backgroundPosition: "center center",
+                  backgroundSize: "cover",
+                  marginBottom: 4
+                }}
+              />
+              <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero necessitatibus, optio autem eligendi quas iure, laudantium assumenda recusandae, molestias exercitationem quos? Aperiam est accusamus at dolore ullam vero, maiores enim minima! Officia nihil amet dolorem voluptatem, incidunt, maxime vel voluptates blanditiis quae odio magnam qui impedit mollitia. Officiis, nisi?
+              </Typography>
+              <br />
+              <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero necessitatibus, optio autem eligendi quas iure, laudantium assumenda recusandae, molestias exercitationem quos? Aperiam est accusamus at dolore ullam vero, maiores enim minima! Officia nihil amet dolorem voluptatem, incidunt, maxime vel voluptates blanditiis quae odio magnam qui impedit mollitia. Officiis, nisi?
+              </Typography>
+            </Box>
+
+            <Box>
+              <h1>Our Coordinators</h1>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: collapsedView ? "row" : "column",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  paddingRight: 10,
+                  paddingLeft: 10,
+                  gap: 5
+                }}
+              >
+                {coordinators.map(coord => (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      gap: 2,
+                      justifyContent: "space-around"
+                    }}
+                  >
+                    <Avatar
+                      alt={coord.alt}
+                      src={coord.src}
+                      sx={{
+                        width: 300,
+                        height: 300
+                      }}
+                    />
+                    <Typography
+                      fontSize={25}
+                      fontWeight={500}
+                    >
+                    {coord.alt}
+                    </Typography>
+                  </Box>
+                ))}
+              </Box>
             </Box>
           </Box>
         </Box>
